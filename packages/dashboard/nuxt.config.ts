ca20e93b15aa94480d979cdc8c593609c3a72287
ivomarsan <ivomarlucio@gmail.com>
2022-09-07 23:29:29 -0300

    Init monorepo structure

---
+++ packages/dashboard/nuxt.config.ts
@@ -0,0 +1,14 @@
+import { defineNuxtConfig } from "nuxt";
+import BaseTSConfig from "./tsconfig.nuxt.json";
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  ssr: false,
+
+  typescript: {
+    shim: true,
+    strict: true,
+    tsConfig: BaseTSConfig,
+    typeCheck: true,
+  },
+});
