e0f3af4ba80d38390cc0988293f086f3a9be76a6
ivomarsan <ivomarlucio@gmail.com>
2023-06-28 20:21:22 -0300

    Create HeaderMenu

---
+++ packages/landing-page-1/components/Header/HeaderMenu.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav :class="$style.container">
+    <ul>
+      <li>Item 1</li>
+      <li>Item 2</li>
+      <li>Item 3</li>
+    </ul>
+  </nav>
+</template>
+
+<style module>
+.container {
+  @apply flex;
+}
+</style>
