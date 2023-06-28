21f98ed55c5e45da5f615152386c854c60a23c6d
ivomarsan <ivomarlucio@gmail.com>
2023-06-28 20:00:02 -0300

    Create HeaderCover

---
+++ packages/landing-page-1/components/Header/HeaderCover.vue
@@ -0,0 +1,21 @@
+<template>
+  <div :class="$style.container">
+    <div :class="$style.inner">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style module>
+.container {
+  background-image: url("@/assets/images/background-header-cover.jpg");
+  background-position: 50%;
+  background-size: cover;
+}
+
+.inner {
+  background-image: url("@/assets/images/background-header-cover.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+</style>
