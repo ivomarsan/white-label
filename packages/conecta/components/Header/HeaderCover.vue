c029a659c3319b5163b3f79b6a48fd23db53479b
ivomarsan <ivomarlucio@gmail.com>
2023-06-28 22:54:08 -0300

    Add isFullscreen prop

--- packages/conecta/components/Header/HeaderCover.vue
+++ packages/conecta/components/Header/HeaderCover.vue
@@ -1,5 +1,26 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+interface Props {
+  fullscreen?: boolean;
+}
+
+const { fullscreen = false } = defineProps<Props>();
+
+const isFullscreen = computed<boolean>(() => {
+  return !!fullscreen;
+});
+</script>
+
 <template>
-  <div :class="$style.container">
+  <div
+    :class="[
+      $style.container,
+      {
+        [$style.isFullscreen]: isFullscreen,
+      },
+    ]"
+  >
     <div :class="$style.inner">
       <slot />
     </div>
@@ -8,14 +29,17 @@
 
 <style module>
 .container {
-  background-image: url("@/assets/images/background-header-cover.jpg");
+  background-image: url('@/assets/images/background-header-cover.jpg');
   background-position: 50%;
   background-size: cover;
+  background-repeat: no-repeat;
+
+  &.isFullscreen {
+    min-height: 100vh;
+  }
 }
 
 .inner {
-  background-image: url("@/assets/images/background-header-cover.jpg");
-  background-size: cover;
-  background-repeat: no-repeat;
+  @apply flex flex-col;
 }
 </style>
