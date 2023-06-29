3566211f71645e200604f6d7878166132fafc30c
ivomarsan <ivomarlucio@gmail.com>
2023-06-28 22:54:40 -0300

    Set fullscreen on HeaderCover

--- packages/conecta/components/Header/Header.vue
+++ packages/conecta/components/Header/Header.vue
@@ -10,13 +10,20 @@
       <HeaderCTA />
     </header>
 
-    <HeaderCover>
-      <h1>Teste</h1>
+    <HeaderCover
+      :class="$style.cover"
+      fullscreen
+    >
+      <h1>Treinamento para vida</h1>
     </HeaderCover>
   </section>
 </template>
 
 <style module>
+:root {
+  --header-height: 7rem;
+}
+
 .container {
   @apply flex flex-col;
 }
@@ -27,7 +34,13 @@
   position: fixed;
   left: 0;
   right: 0;
-  background-color: red;
+  height: var(--header-height);
+  max-width: 1600px;
+  margin: 0 auto;
+}
+
+.cover {
+  padding-top: var(--header-height);
 }
 
 .menu {
