59941831bfd9f8d1f9ce17ef3e9def83275bcdd1
ivomarsan <ivomarlucio@gmail.com>
2023-06-28 22:15:34 -0300

    Create HeaderCTA

---
+++ packages/conecta/components/Header/HeaderCTA.vue
@@ -0,0 +1,15 @@
+<template>
+  <Button
+    :class="$style.cta"
+    color-background="#05005b"
+    color-text="#cccccc"
+    enable-hover-effect
+    outline
+  />
+</template>
+
+<style module>
+.cta {
+  @apply flex;
+}
+</style>
