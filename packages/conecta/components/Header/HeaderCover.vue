c2acd5a616eb46794062c8f8c5775e928aa6ffdf
ivomarsan <ivomarlucio@gmail.com>
2023-06-28 23:20:36 -0300

    Add CurveShape SVG

--- packages/conecta/components/Header/HeaderCover.vue
+++ packages/conecta/components/Header/HeaderCover.vue
@@ -1,14 +1,20 @@
 <script setup lang="ts">
 import { computed } from 'vue';
+import CurveShapeSVG from '@/assets/svgs/curve-shape.svg';
 
 interface Props {
   fullscreen?: boolean;
+  enableCurveShape?: boolean;
 }
 
-const { fullscreen = false } = defineProps<Props>();
+const { fullscreen = false, enableCurveShape = false } = defineProps<Props>();
 
 const isFullscreen = computed<boolean>(() => {
   return !!fullscreen;
+});
+
+const isCurveShapeVisible = computed<boolean>(() => {
+  return !!enableCurveShape;
 });
 </script>
 
@@ -24,11 +30,20 @@ const isFullscreen = computed<boolean>(() => {
     <div :class="$style.inner">
       <slot />
     </div>
+
+    <div
+      v-if="isCurveShapeVisible"
+      :class="$style.curveShape"
+    >
+      <CurveShapeSVG />
+    </div>
   </div>
 </template>
 
 <style module>
 .container {
+  @apply flex flex-col justify-between;
+
   background-image: url('@/assets/images/background-header-cover.jpg');
   background-position: 50%;
   background-size: cover;
