e42741336a9a5b00b2ea3e45c7ed161d9d9064cb
ivomarsan <ivomarlucio@gmail.com>
2023-06-28 22:14:43 -0300

    Add enableHoverEffect prop

--- packages/conecta/components/Button.vue
+++ packages/conecta/components/Button.vue
@@ -5,23 +5,35 @@ interface Props {
   colorBackground?: string;
   colorText?: string;
   outline?: boolean;
+  enableHoverEffect?: boolean;
 }
 
 const {
   colorBackground = '#338033',
   colorText = '#cccccc',
   outline = false,
+  enableHoverEffect = false,
 } = defineProps<Props>();
 
 const isOutline = computed<boolean>(() => {
   return !!outline;
+});
+
+const isHoverEffectEnabled = computed<boolean>(() => {
+  return !!enableHoverEffect;
 });
 </script>
 
 <template>
   <div :class="$style.container">
     <button
-      :class="[$style.button, { [$style.isOutline]: isOutline }]"
+      :class="[
+        $style.button,
+        {
+          [$style.isOutline]: isOutline,
+          [$style.isHoverEffectEnabled]: isHoverEffectEnabled,
+        },
+      ]"
       type="button"
     >
       <span>Hover Me</span>
@@ -35,7 +47,7 @@ const isOutline = computed<boolean>(() => {
 }
 
 .button {
-  background: v-bind(colorBackground);
+  background-color: v-bind(colorBackground);
   position: relative;
   color: v-bind(colorText);
   min-width: 10rem;
@@ -43,61 +55,68 @@ const isOutline = computed<boolean>(() => {
   border: none;
   font-size: 18px;
   border-radius: 4px;
-  transition: 600ms;
+  transition: 300ms;
   overflow: hidden;
 
   &:focus {
     outline: none;
   }
 
-  &::before {
-    content: '';
-    display: block;
-    position: absolute;
-    background: rgba(255, 255, 255, 0.5);
-    width: 60px;
-    height: 100%;
-    left: 0;
-    top: 0;
-    opacity: 0.5;
-    filter: blur(30px);
-    transform: translateX(-130px) skewX(-15deg);
-  }
-
-  &::after {
-    content: '';
-    display: block;
-    position: absolute;
-    background: rgba(255, 255, 255, 0.2);
-    width: 30px;
-    height: 100%;
-    left: 30px;
-    top: 0;
-    opacity: 0;
-    filter: blur(30px);
-    transform: translate(-100px) scaleX(-15deg);
-  }
-
   &:hover {
-    background: v-bind(colorBackground);
     cursor: pointer;
-
-    &::before {
-      transform: translateX(300px) skewX(-15deg);
-      opacity: 0.6;
-      transition: 700ms;
-    }
-
-    &::after {
-      transform: translateX(300px) skewX(-15deg);
-      opacity: 1;
-      transition: 700ms;
-    }
   }
 
   &.isOutline {
     border: 1px solid v-bind(colorBackground);
-    background: none;
+    background-color: transparent;
+
+    &:hover {
+      background-color: v-bind(colorBackground);
+    }
+  }
+
+  &.isHoverEffectEnabled {
+    &::before {
+      content: '';
+      display: block;
+      position: absolute;
+      background-color: rgba(255, 255, 255, 0.5);
+      width: 60px;
+      height: 100%;
+      left: 0;
+      top: 0;
+      opacity: 0.5;
+      filter: blur(30px);
+      transform: translateX(-130px) skewX(-15deg);
+    }
+
+    &::after {
+      content: '';
+      display: block;
+      position: absolute;
+      background-color: rgba(255, 255, 255, 0.2);
+      width: 30px;
+      height: 100%;
+      left: 30px;
+      top: 0;
+      opacity: 0;
+      filter: blur(30px);
+      transform: translate(-100px) scaleX(-15deg);
+    }
+
+    &:hover {
+      &::before {
+        transform: translateX(300px) skewX(-15deg);
+        opacity: 0.6;
+        transition: 700ms;
+      }
+
+      &::after {
+        transform: translateX(300px) skewX(-15deg);
+        opacity: 1;
+        transition: 700ms;
+      }
+    }
   }
 }
 </style>
