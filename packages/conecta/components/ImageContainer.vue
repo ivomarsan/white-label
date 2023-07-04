8e06348c52595a8a80657820cd65c927f354fb80
ivomarsan <ivomarlucio@gmail.com>
2023-07-04 20:16:56 -0300

    Update border-width

--- packages/conecta/components/ImageContainer.vue
+++ packages/conecta/components/ImageContainer.vue
@@ -76,12 +76,12 @@ function getImage(src: Props['src']) {
 
   &.hasBorderLeft {
     border-radius: 16px 16px 16px 15vw;
-    border-left-width: 2vw;
+    border-left-width: 1vw;
   }
 
   &.hasBorderRight {
     border-radius: 16px 16px 15vw 16px;
-    border-right-width: 2vw;
+    border-right-width: 1vw;
   }
 }
 </style>
