d37253476418098a8c5cc3f547124a6bd44ed4b2
ivomarsan <ivomarlucio@gmail.com>
2023-07-04 18:02:53 -0300

    Create ImageContainer

---
+++ packages/conecta/components/ImageContainer.vue
@@ -0,0 +1,84 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+type Border = 'left' | 'right' | 'none';
+
+type BoxShadow = 'on' | 'off';
+
+interface Props {
+  border?: Border;
+  boxShadow?: BoxShadow;
+  src?: string;
+  alt?: string;
+}
+
+const { border = 'none', boxShadow = 'off', src, alt } = defineProps<Props>();
+
+const hasBorder = computed<boolean>(() => {
+  return border !== 'none';
+});
+
+const hasBoxShadow = computed<boolean>(() => {
+  return boxShadow === 'off';
+});
+
+const hasBorderLeft = computed<boolean>(() => {
+  return !!hasBorder.value && border === 'left';
+});
+
+const hasBorderRight = computed<boolean>(() => {
+  return !!hasBorder.value && border === 'right';
+});
+
+function getImage(src: Props['src']) {
+  if (!src) {
+    return '';
+  }
+
+  return new URL(`../assets/${src}`, import.meta.url).href;
+}
+</script>
+
+<template>
+  <div
+    :class="[
+      $style.container,
+      {
+        [$style.hasBoxShadow]: hasBoxShadow,
+        [$style.hasBorder]: hasBorder,
+        [$style.hasBorderLeft]: hasBorderLeft,
+        [$style.hasBorderRight]: hasBorderRight,
+      },
+    ]"
+  >
+    <img
+      :alt="alt"
+      :src="getImage(src)"
+    />
+  </div>
+</template>
+
+<style module>
+.container {
+  @apply flex;
+
+  &.hasBoxShadow {
+    box-shadow: 0px 2px 18px 0px rgba(0, 0, 0, 0.23);
+  }
+
+  &.hasBorder {
+    overflow: hidden;
+    border-color: #05005b;
+  }
+
+  &.hasBorderLeft {
+    border-radius: 30px 0 0 15vw;
+    border-left-width: 2vw;
+  }
+
+  &.hasBorderRight {
+    border-radius: 0 30px 15vw 0;
+    border-right-width: 2vw;
+  }
+}
+</style>
