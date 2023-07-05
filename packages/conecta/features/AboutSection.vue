f3d55823de8ad0881a7542cb60ec897498c300e9
ivomarsan <ivomarlucio@gmail.com>
2023-07-05 01:21:34 -0300

    Create AboutSection

---
+++ packages/conecta/features/AboutSection.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import SectionContainer from '@/components/Section/SectionContainer.vue';
+import ImageContainer from '@/components/ImageContainer.vue';
+
+const description =
+  'Conectamos empresas e pessoas com os nossos especialistas para elaborar um Plano de Treinamento e fazer a ponte com os parceiros que vão colocá-lo em prática. Nós ajudamos empresas a estarem em conformidade com a lei e fazerem um melhor  uso de seus recursos, ao mesmo tempo em que garantimos uma experiência de  qualidade através de nossos parceiros credenciados.';
+</script>
+
+<template>
+  <SectionContainer
+    background-color="#afbedb"
+    :description="description"
+    title="Quem somos"
+    title-align="left"
+  >
+    <template #image>
+      <ImageContainer
+        border="left"
+        box-shadow="on"
+        src="images/helping-hands.jpg"
+      />
+    </template>
+  </SectionContainer>
+</template>
+
+<style module></style>
--- packages/conecta/pages/index.vue
+++ packages/conecta/pages/index.vue
@@ -3,8 +3,9 @@ import BlogSection from '@/features/BlogSection.vue';
 import FooterSection from '@/features/FooterSection.vue';
 import HeaderSection from '@/features/HeaderSection.vue';
 import MissionSection from '@/features/MissionSection.vue';
-import PurposeSection from '@/features/PurposeSection.vue';
+// import PurposeSection from '@/features/PurposeSection.vue';
 import ServicesSection from '@/features/ServicesSection.vue';
+import AboutSection from '@/features/AboutSection.vue';
 import TestimonialsSection from '@/features/TestimonialsSection.vue';
 </script>
 
@@ -12,7 +13,8 @@ import TestimonialsSection from '@/features/TestimonialsSection.vue';
   <main>
     <HeaderSection />
 
-    <PurposeSection />
+    <!-- <PurposeSection /> -->
+    <AboutSection />
 
     <MissionSection />
 
