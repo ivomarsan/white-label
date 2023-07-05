1f161abef7e0dfa88240a0c0ff823b2a761ce26d
ivomarsan <ivomarlucio@gmail.com>
2023-07-05 00:31:43 -0300

    Add buttons

--- packages/conecta/components/Carousel.vue
+++ packages/conecta/components/Carousel.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import ImageContainer from '@/components/ImageContainer.vue';
+import ArrowRightSVG from '@/assets/svgs/arrow-right.svg';
+import ArrowLeftSVG from '@/assets/svgs/arrow-left.svg';
+import { useTheme } from '@/composables';
 
 type MediaType = 'image' | 'video';
 
@@ -15,13 +18,19 @@ interface Props {
   items?: CarouselItem[];
   blurInactives?: boolean;
   expandActive?: boolean;
+  showControls?: boolean;
 }
 
 const {
   items,
   blurInactives = false,
   expandActive = true,
+  showControls = false,
 } = defineProps<Props>();
+
+const { themeColors } = useTheme();
+
+const currentCaroulselItemIndex = ref<number>(0);
 
 const isEmpty = computed<boolean>(() => {
   if (Array.isArray(items)) {
@@ -39,8 +48,40 @@ const isActiveItemExpanded = computed<boolean>(() => {
   return !!expandActive;
 });
 
+const areControlsVisible = computed<boolean>(() => {
+  return !!showControls;
+});
+
 function isActiveItem(index: number) {
-  return index === 1;
+  return index === currentCaroulselItemIndex.value;
+}
+
+function onPrevButtonClick() {
+  if (!items?.length) {
+    return;
+  }
+
+  const lastItem = items?.length - 1;
+
+  if (currentCaroulselItemIndex.value === 0) {
+    currentCaroulselItemIndex.value = lastItem;
+  } else {
+    currentCaroulselItemIndex.value--;
+  }
+}
+
+function onNextButtonClick() {
+  if (!items?.length) {
+    return;
+  }
+
+  const lastItem = items?.length - 1;
+
+  if (currentCaroulselItemIndex.value === lastItem) {
+    currentCaroulselItemIndex.value = 0;
+  } else {
+    currentCaroulselItemIndex.value++;
+  }
 }
 </script>
 
@@ -77,6 +118,22 @@ function isActiveItem(index: number) {
           src="images/feeling-safe.jpg"
         />
       </template>
+
+      <template v-if="areControlsVisible">
+        <button
+          :class="[$style.controlButton, $style.left]"
+          @click="onPrevButtonClick"
+        >
+          <ArrowLeftSVG />
+        </button>
+
+        <button
+          :class="[$style.controlButton, $style.right]"
+          @click="onNextButtonClick"
+        >
+          <ArrowRightSVG />
+        </button>
+      </template>
     </template>
   </div>
 </template>
@@ -84,7 +141,9 @@ function isActiveItem(index: number) {
 <style module>
 .carousel {
   @apply flex;
-  @apply gap-4;
+  @apply gap-4 px-16;
+
+  position: relative;
 
   &.isEmpty {
     @apply font-bold;
@@ -92,6 +151,8 @@ function isActiveItem(index: number) {
 }
 
 .item {
+  transition: all 100ms ease-in-out;
+
   &.isInactive {
     z-index: 0;
 
@@ -106,6 +167,32 @@ function isActiveItem(index: number) {
     &.isExpanded {
       transform: scale(1.15);
     }
+  }
+}
+
+.controlButton {
+  @apply flex items-center justify-center;
+
+  height: 48px;
+  width: 48px;
+  border-radius: 50%;
+  background-color: #cfcdcd;
+  z-index: 1;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+
+  & svg {
+    width: 32px;
+    fill: v-bind('themeColors.primary');
+  }
+
+  &.left {
+    left: 0;
+  }
+
+  &.right {
+    right: 0;
   }
 }
 </style>
