378f91b1b12598260dc2a51af6d81fad62cf8f16
ivomarsan <ivomarlucio@gmail.com>
2023-07-07 04:37:28 -0300

    Create blog page

---
+++ packages/conecta/pages/blog.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import BlogSection from '@/features/BlogSection.vue';
+// import FooterSection from '@/features/FooterSection.vue';
+import Header from '@/components/Header/Header.vue';
+</script>
+
+<template>
+  <main>
+    <Header hide-cover />
+
+    <BlogSection />
+
+    <!-- <FooterSection /> -->
+  </main>
+</template>
+
+<style module></style>
