7e9f5c9593fe47b6080ad4ecd2d11729599deee0
ivomarsan <ivomarlucio@gmail.com>
2023-07-07 01:38:32 -0300

    Replace logo

--- packages/conecta/components/Header/HeaderLogo.vue
+++ packages/conecta/components/Header/HeaderLogo.vue
@@ -1,17 +1,25 @@
+<script setup lang="ts">
+import LogoSVG from '@/assets/svgs/logo.svg';
+import { useTheme } from '@/composables';
+
+const { themeColors } = useTheme();
+</script>
+
 <template>
   <div :class="$style.logo">
-    <img
-      alt="Conecta Logo"
-      src="@/src/logo.png"
-    />
+    <LogoSVG />
   </div>
 </template>
 
 <style module>
 .logo {
-  @apply flex;
+  @apply flex h-full;
   @apply m-2;
 
   max-width: 15rem;
+
+  & svg {
+    fill: v-bind('themeColors.secondary');
+  }
 }
 </style>
