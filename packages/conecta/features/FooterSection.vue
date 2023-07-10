367b80e1deb52018a568b7d4fa022b1063b80ddd
ivomarsan <ivomarlucio@gmail.com>
2023-07-10 03:58:52 -0300

    Refact Footer

--- packages/conecta/features/FooterSection.vue
+++ packages/conecta/features/FooterSection.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
+import SectionContainer from '@/components/Section/SectionContainer.vue';
 import Footer from '@/components/Footer.vue';
+import { useTheme } from '@/composables';
+
+const { themeColors } = useTheme();
 </script>
 
 <template>
-  <Footer />
+  <SectionContainer :background-color="themeColors.primary">
+    <template #container>
+      <Footer />
+    </template>
+  </SectionContainer>
 </template>
 
 <style module></style>
